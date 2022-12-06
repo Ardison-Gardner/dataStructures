3a727798600071e852b5d6b9f87e99fe2504169b
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-12-05 23:46:07 -0500

    Arrays.

---
+++ dataStructures.js
@@ -0,0 +1,10 @@
+//Javascript arrays
+
+const arr = [1, 2, 3]
+
+const indexOfTwo = arr.indexOf(2);
+console.log(arr[indexOfTwo-1]); // 1
+console.log(arr[indexOfTwo+1]); // 3
+
+arr.push(4);
+console.log(arr) // [1,2,3,4];
